ab5f911b087562ed81f03c132d33dfaa679a06a5
James McFeeters <jameeters@gmail.com>
2017-06-13 16:26:49 -0500

    Wrap logic in function and module
    Move all of `site_listing.py` to within `get_sites()`, which takes a
    state postal code (eg MD) as a parameter and returns a list of dicts
    describing the USGS stream gages in that state.
    
    Add `__init__.py` to make the folder a python module and allow it to
    be imported with `from stream_gages.site_listing import get_sites`.
    
    Add `.gitignore` copied from the github template.

--- site_listing.py
+++ site_listing.py
@@ -1,29 +1,30 @@
 import requests
-import csv
-from pprint import pprint
 
-url = "https://waterservices.usgs.gov/nwis/site?format=rdb&stateCd=mo&outputDataTypeCd=iv,dv&siteType=ST&siteStatus=active&hasDataTypeCd=iv,dv"
+def get_sites(state):
+	url = 'https://waterservices.usgs.gov/nwis/site?format=rdb&stateCd={}&outputDataTypeCd=iv,dv&siteType=ST&siteStatus=active&hasDataTypeCd=iv,dv'.format(state.lower())
 
-req = requests.get(url)
+	req = requests.get(url)
+	if req.status_code != 200 :
+		print('request has returned with status {}'.format(req.status_code))
+		return
 
-# Discard preamble from response
-data = req.text[ req.text.rindex('#') + 1 : ]
-data = data[ data.index('\n') + 1 : ]
-# the first line of data is now the column headers
+	# Discard preamble from response
+	data = req.text[ req.text.rindex('#') + 1 : ]
+	data = data[ data.index('\n') + 1 : ]
+	# the first line of data is now the column headers
 
-# turn data into list by splitting on \n
-data = data.split('\n')
+	# turn data into list by splitting on \n
+	data = data.split('\n')
 
-# discard 2nd line
-data = [data[0]] + data[2:]
+	# discard 2nd line
+	data = [data[0]] + data[2:]
 
-print(len(data))
-fields = data[0].split('\t')
-data_nice = []
-for line in data[1:]:
-	line = line.split('\t')
-	line_dict = dict(zip(fields, line))
-	data_nice.append(line_dict)
+	fields = data[0].split('\t')
+	data_nice = []
+	for line in data[1:]:
+		line = line.split('\t')
+		line_dict = dict(zip(fields, line))
+		data_nice.append(line_dict)
 
-print(len(data_nice))
-pprint(data_nice[0])		
+	return (data_nice)
+
