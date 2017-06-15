cb5e0ea0c4023517037c8c17f5c0616f6bec99bd
James McFeeters <jmcfeeters@usgs.gov>
2017-06-15 14:25:31 -0500

    Finish basic site detail fetching script

--- gage_details.py
+++ gage_details.py
@@ -13,18 +13,18 @@ def get_details (site, start_date, end_date):
     resp_data = json.loads(req.text)
     # I need the data itself, the bad value, unit, variable description and name
     # discard unneeded data
-        resp_data = resp_data['value']['timeSeries'][0]
+    resp_data = resp_data['value']['timeSeries'][0]
 
-    # begin building what I'll return       
-        good_data = {}
+    # begin building what I'll return
+    good_data = {}
     # I will use this to keep track of any problems that I don't want to stop on
-        good_data['problems'] = []
+    good_data['problems'] = []
 
     # get site number (I want to be sure they match)
     site_num = resp_data['sourceInfo']['siteCode'][0]['value']
     good_data['site_num'] = site_num
     if site_num != site :
-        good_data['problems'].append('Site number in response ({}) does not match given ({})'.format(site_num, site)
+        good_data['problems'].append('Site number in response {} does not match given {}'.format(site_num, site))
 
     # get site name
     good_data['siteName'] = resp_data['sourceInfo']['siteName']
@@ -43,9 +43,11 @@ def get_details (site, start_date, end_date):
     # get datapoints
     good_data['series'] = []
     for item in resp_data['values'][0]['value'] :
-        if int(item['value']) == int(no_value):
+        if item['value'] == no_value:
             item['value'] = None
         del item['qualifiers']
         good_data['series'].append(item)
+
+    return good_data
 
 
